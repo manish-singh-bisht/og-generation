7b7e762385af96d9caf6c39c1ec5c26b3f490540
manish-singh-bisht <mthefool218@gmail.com>
2024-08-07 01:37:44 +0530

    dockerfile puppetter and something

--- backend/src/utils/images/ogGenerate.js
+++ backend/src/utils/images/ogGenerate.js
@@ -2,7 +2,18 @@ const puppeteer = require("puppeteer");
 const ogTemplate = require("../template/ogTemplate");
 
 async function generateOgImage(title, content, image) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
   const page = await browser.newPage();
   await page.setViewport({ width: 1200, height: 630 });
   await page.setContent(ogTemplate("medial_username_1", title, content, image));
